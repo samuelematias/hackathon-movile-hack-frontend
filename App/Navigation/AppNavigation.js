fdd5a58d115b63d5734061b613256e462cf9c03b
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2019-02-10 06:14:12 -0300

    adding login to first screen of stack

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -66,7 +66,7 @@ const AppNavigation = StackNavigator(
 	},
 	{
 		headerMode: Platform.OS === 'ios' ? 'float' : 'screen',
-		initialRouteName: 'TabRoot',
+		initialRouteName: 'Login',
 		/* The header config from HomeScreen is now here */
 		navigationOptions: {
 			headerStyle: {
